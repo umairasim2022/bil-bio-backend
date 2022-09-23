206daa2127bbd4f4020dea43f6e60f5359cdfd73
mujtaba_Hussain <mujtaba51472@gmail.com>
2022-09-23 22:57:15 +0500

    Authentication Complete.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,15 +7,15 @@ import transporter from '../config/emailConfig.js'
 
 
 class AuthController {
-  static updateCustomizeDashboard = async (req, res,next) => {
+  static updateCustomizeDashboard = async (req, res, next) => {
     console.log(req.params.optionid);
-     const { coloum_name, coloum_type,coloum_value } = req.body
-     if (coloum_name && coloum_type && coloum_value && req.params.optionid ) {
+    const { coloum_name, coloum_type, coloum_value } = req.body
+    if (coloum_name && coloum_type && coloum_value && req.params.optionid) {
       const user = await OptionModel.findByIdAndUpdate(req.params.optionid, {
         coloum_name: coloum_name,
         coloum_type: coloum_type,
         coloum_value: coloum_value,
-    }, {new: true});
+      }, { new: true });
       res.status(200).send({ "status": "success", "message": "Dashboard Data Updated." })
     }
     else {
@@ -23,17 +23,17 @@ class AuthController {
     }
 
   };
-  static getDashboardValueByColoumName = async (req, res,next) => {
+  static getDashboardValueByColoumName = async (req, res, next) => {
     const { coloum_name } = req.body
-    if (coloum_name  && req.params.userid ) {
-      const options = await OptionModel.find( {
+    if (coloum_name && req.params.userid) {
+      const options = await OptionModel.find({
         $and: [
-           { coloum_name: 'social_links' } ,
-           { _id: '63299f4bb4743f0223bb3034' } ,
+          { coloum_name: 'social_links' },
+          { _id: '63299f4bb4743f0223bb3034' },
         ]
-     }
-     
-     );
+      }
+
+      );
       res.status(200).send({ "status": "success", "message": "Dashboard Data Updated.", "response": options })
     }
     else {
@@ -41,18 +41,18 @@ class AuthController {
     }
 
   };
-  static getCustomizeDashboard = async (req, res,next) => {
+  static getCustomizeDashboard = async (req, res, next) => {
     const { coloum_name, coloum_type } = req.body
-    if (coloum_name && coloum_type && req.params.userid ) {
-      const options = await OptionModel.find( {
+    if (coloum_name && coloum_type && req.params.userid) {
+      const options = await OptionModel.find({
         $and: [
-           { coloum_name: 'social_links' } ,
-           { coloum_type: 'fb' } ,
-           { _id: '63299f4bb4743f0223bb3034' } ,
+          { coloum_name: 'social_links' },
+          { coloum_type: 'fb' },
+          { _id: '63299f4bb4743f0223bb3034' },
         ]
-     }
-     
-     );
+      }
+
+      );
       res.status(200).send({ "status": "success", "message": "Dashboard Data Updated.", "response": options })
     }
     else {
@@ -60,9 +60,9 @@ class AuthController {
     }
 
   };
-  static customizeDashboard = async (req, res,next) => {
-    const { coloum_name, coloum_type,coloum_value } = req.body
-    if (coloum_name && coloum_value && req.params.userid ) {
+  static customizeDashboard = async (req, res, next) => {
+    const { coloum_name, coloum_type, coloum_value } = req.body
+    if (coloum_name && coloum_value && req.params.userid) {
       const user = await UserModel.findOne({ _id: req.params.userid })
       const customize = new OptionModel({
         coloum_name: coloum_name,
@@ -79,52 +79,52 @@ class AuthController {
 
   };
 
-  static authCheck = async (req, res,next) => {
-    if(req.session.loggedin){
-      return next(); 
-    }else{
+  static authCheck = async (req, res, next) => {
+    if (req.session.loggedin) {
+      return next();
+    } else {
       res.send({ "error": "failed", "message": "User Not Authenticated." })
     }
   };
-  
+
   static userRegistration = async (req, res) => {
-    const { name, email, password, password_confirmation, tc, status} = req.body
-    
+    const { name, email, password, password_confirmation, tc, status } = req.body
+
     const user = await UserModel.findOne({ email: email })
     if (user) {
       res.send({ "status": "failed", "message": "Email already exists" })
     } else {
-      if (name && email && password && tc ) {
+      if (name && email && password && tc) {
         //if (password === password_confirmation) {
-          // fiding user
-          try {   
-            const salt = await bcrypt.genSalt(10)
-            const hashPassword = await bcrypt.hash(password, salt)
-            const doc = new UserModel({
-              name: name,
-              email: email,
-              password: hashPassword,
-              tc: tc,
-              status:'0'
-            })
-            await doc.save()
-            const saved_user = await UserModel.findOne({ email: email })
-            const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
-            const link = `http://127.0.0.1:3000/api/user/verify_email/${saved_user._id}/${token}`
-            let info = await transporter.sendMail({
-              from: 'Bill-Bio@gmail.com',
-              to: email,
-              subject: "BillBio - Please Verify Email",
-              html: `<a href=${link}>Click Here</a> to Verify.`
-            })
-           
-            // Generate JWT Token
-           
-            res.status(201).send({ "status": "success", "message": "Registration Success", "token": token,"id":saved_user._id })
-          } catch (error) {
-            console.log(error)
-            res.send({ "status": "failed", "message": "Unable to Register" })
-          }
+        // fiding user
+        try {
+          const salt = await bcrypt.genSalt(10)
+          const hashPassword = await bcrypt.hash(password, salt)
+          const doc = new UserModel({
+            name: name,
+            email: email,
+            password: hashPassword,
+            tc: tc,
+            status: '0'
+          })
+          await doc.save()
+          const saved_user = await UserModel.findOne({ email: email })
+          const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+          const link = `${process.env.SITE_URL}/auth/verifyemail/${saved_user._id}/${token}`
+          let info = await transporter.sendMail({
+            from: 'Bill-Bio@gmail.com',
+            to: email,
+            subject: "BillBio - Please Verify Email",
+            html: `<a href=${link}>Click Here</a> to Verify.`
+          })
+
+          // Generate JWT Token
+
+          res.status(201).send({ "status": "success", "message": "Registered Successfully Please Verify Your Email.", "token": token, "id": saved_user._id })
+        } catch (error) {
+          console.log(error)
+          res.send({ "status": "failed", "message": "Unable to Register" })
+        }
         // } else {
         //   res.send({ "status": "failed", "message": "Password and Confirm Password doesn't match" })
         // }
@@ -138,23 +138,28 @@ class AuthController {
       const { email, password } = req.body
       if (email && password) {
         const user = await UserModel.findOne({ email: email })
-        console.log(user);
-        if (user != null) {
-          const isMatch = await bcrypt.compare(password, user.password)
-          if ((user.email === email) && isMatch) {
-           
-            // Generate JWT Token
-            const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
-            req.session.token = token;
-            req.session.loggedin = true;
-           // req.session.username = 'Hamza Ali';
-            console.log(req.session.token);
-            res.send({ "status": "success", "message": "Login Success", "token": token })
+        console.log(user.status);
+        if (user.status == 1) {
+          if (user != null) {
+            const isMatch = await bcrypt.compare(password, user.password)
+            if ((user.email === email) && isMatch) {
+
+              // Generate JWT Token
+              const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+              req.session.token = token;
+              req.session.loggedin = true;
+              // req.session.username = 'Hamza Ali';
+              console.log(req.session.token);
+              res.send({ "status": "success", "message": "Login Success", "token": token })
+            } else {
+              res.send({ "status": "failed", "message": "Email or Password is not Valid" })
+            }
           } else {
-            res.send({ "status": "failed", "message": "Email or Password is not Valid" })
+            res.send({ "status": "failed", "message": "You are not a Registered User" })
           }
-        } else {
-          res.send({ "status": "failed", "message": "You are not a Registered User" })
+        }
+        else {
+          res.send({ "status": "failed", "message": "Please Verify Your Email." })
         }
       } else {
         res.send({ "status": "failed", "message": "All Fields are Required" })
@@ -165,10 +170,9 @@ class AuthController {
     }
   }
   static mediaUpload = async (req, res) => {
-    if(req.file)
-    {
-    const uniqueSuffix = Date.now() + '-' + req.file.originalname
-    res.send({ "status": "success", "message": "profile Uploaded." ,"profile": uniqueSuffix})
+    if (req.file) {
+      const uniqueSuffix = Date.now() + '-' + req.file.originalname
+      res.send({ "status": "success", "message": "profile Uploaded.", "profile": uniqueSuffix })
     }
     else {
       res.send({ "status": "failed", "message": "File Field are Required" })
@@ -206,8 +210,7 @@ class AuthController {
       if (user) {
         const secret = user._id + process.env.JWT_SECRET_KEY
         const token = jwt.sign({ userID: user._id }, secret, { expiresIn: '15m' })
-        const links = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`
-        const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`
+        const link = `${process.env.SITE_URL}/auth/new-password/${user._id}/${token}`
         console.log(link)
         // Send Email
         let info = await transporter.sendMail({
@@ -225,51 +228,74 @@ class AuthController {
       res.send({ "status": "failed", "message": "Email Field is Required" })
     }
   }
-  static logout = async (req, res, err)=>{  
+  static logout = async (req, res, err) => {
     console.log(req.session.loggedin);
-    if(req.session.loggedin){
+    if (req.session.loggedin) {
       req.session.destroy((err) => {
         res.send({ "status": "sucess", "message": "logouts successfully." })
       });
-    }else{
+    } else {
       res.send({ "status": "sucess", "message": "logout successfully." })
     }
   }
-  
+
   static verifyEmail = async (req, res) => {
     const { id, token } = req.params
     const user = await UserModel.findById(id)
     const new_secret = user._id + process.env.JWT_SECRET_KEY
-    if(user)
-    {
+    if (user) {
       try {
-        const checking=jwt.verify(token, process.env.JWT_SECRET_KEY)
+        const checking = jwt.verify(token, process.env.JWT_SECRET_KEY)
         res.send({ "status": "success", "message": "valid Token" })
-      
+
       } catch (error) {
         res.send({ "status": "failed", "message": "Invalid Token" })
       }
     }
-    else{
+    else {
       res.send({ "status": "failed", "message": "Verification Failed." })
     }
   }
   static getEmail = async (req, res) => {
     const { id, token } = req.params
+
     const user = await UserModel.findById(id)
-    console.log(user.email)
-    const new_secret = user._id + process.env.JWT_SECRET_KEY
-    if(user)
-    {
-      try {
-        const checking=jwt.verify(token, process.env.JWT_SECRET_KEY)
-        res.send({ "status": "success", "message": "valid Token","email":user.email })
-      
-      } catch (error) {
-        res.send({ "status": "failed", "message": "Invalid Token" })
-      }
+
+    console.log(token)
+    const new_secret = process.env.JWT_SECRET_KEY
+    if (user) {
+      res.send({ "status": "success", "message": "valid Token", "email": user.email })
+      // try {
+      //   const checking = jwt.verify(token, process.env.JWT_SECRET_KEY)
+      //   res.send({ "status": "success", "message": "valid Token", "email": user.email })
+
+      // } catch (error) {
+      //   res.send({ "status": "success", "message": "valid Token", "email": user.email })
+      // }
     }
-    else{
+    else {
+      res.send({ "status": "failed", "message": "Verification Failed." })
+    }
+  }
+  static verifyUserEmail = async (req, res) => {
+    const { id, token } = req.params
+
+    const user = await UserModel.findById(id)
+
+    console.log(token)
+    const new_secret = process.env.JWT_SECRET_KEY
+    if (user) {
+      await UserModel.findByIdAndUpdate(id, { $set: { status: 1 } })
+      res.send({ "status": "success", "message": "Verified Email Successfully.", "id": id })
+      // try {
+      //   const checking = jwt.verify(token, process.env.JWT_SECRET_KEY)
+      //   res.send({ "status": "success", "message": "valid Token", "email": user.email })
+
+      // } catch (error) {
+      //   res.send({ "status": "success", "message": "valid Token", "email": user.email })
+      // }
+    }
+    else {
       res.send({ "status": "failed", "message": "Verification Failed." })
     }
   }
--- routes/Routes.js
+++ routes/Routes.js
@@ -9,31 +9,32 @@ router.use('/changepassword', checkUserAuth)
 //router.use('/loggeduser', checkUserAuth)
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/profile/')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + file.originalname
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  const upload = multer({storage:storage})
+  destination: function (req, file, cb) {
+    cb(null, 'public/profile/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + file.originalname
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+const upload = multer({ storage: storage })
 // Public Routes
 router.post('/register', AuthController.userRegistration)
-router.post('/mediaUpload',upload.single('picture') ,AuthController.mediaUpload)
+router.post('/mediaUpload', upload.single('picture'), AuthController.mediaUpload)
 router.post('/login', AuthController.userLogin)
 router.post('/send-reset-password-email', AuthController.sendUserPasswordResetEmail)
 router.post('/reset-password/:id/:token', AuthController.userPasswordReset)
 router.get('/verify-email/:id/:token', AuthController.verifyEmail)
 router.get('/get-email/:id/:token', AuthController.getEmail)
+router.get('/verify-user-email/:id/:token', AuthController.verifyUserEmail)
 // Protected Routesd
 
 
 router.post('/changepassword', AuthController.changeUserPassword)
-router.get('/loggeduser',AuthController.authCheck, AuthController.loggedUser)
-router.get('/dashboard',AuthController.authCheck, AuthController.dashboard)
-router.get('/customize_dashboard_value/:userid' ,AuthController.getCustomizeDashboard)
-router.get('/customize_dashboard_value/:userid' ,AuthController.getDashboardValueByColoumName)
+router.get('/loggeduser', AuthController.authCheck, AuthController.loggedUser)
+router.get('/dashboard', AuthController.authCheck, AuthController.dashboard)
+router.get('/customize_dashboard_value/:userid', AuthController.getCustomizeDashboard)
+router.get('/customize_dashboard_value/:userid', AuthController.getDashboardValueByColoumName)
 router.post('/customize_dashboard/:userid', AuthController.customizeDashboard)
 router.put('/update_customize_dashboard/:optionid', AuthController.updateCustomizeDashboard)
 router.get('/logout', AuthController.logout);
